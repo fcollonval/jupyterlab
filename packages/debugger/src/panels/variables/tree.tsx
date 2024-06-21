dff45d75d2ecac49a407520ac0e2168aa60a73b0
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-06-21 20:19:09 +0530

    some changes in tree.tsx and variables.css files

--- packages/debugger/src/panels/variables/tree.tsx
+++ packages/debugger/src/panels/variables/tree.tsx
@@ -484,7 +484,7 @@ const VariableComponent = (props: IVariableComponentProps): JSX.Element => {
         <span className="jp-DebuggerVariables-detail">
           {_prepareDetail(variable)}
         </span>
-        {variables && (
+        {variables ? (
           <VariablesBranch
             key={variable.name}
             commands={commands}
@@ -496,7 +496,8 @@ const VariableComponent = (props: IVariableComponentProps): JSX.Element => {
             onHoverChanged={onHoverChanged}
             collapserIcon={collapserIcon}
           />
-        )}
+        ) : expandable && (<TreeItem/>)
+      }
       </TreeItem>
   );
 };
